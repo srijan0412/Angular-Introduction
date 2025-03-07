15d753c1ea607ee0b5455a370f1d94539d0c07e9
srijan0412 <srijan.soni01@gmail.com>
2025-03-07 18:52:17 +0530

    [Srijan] Fix . Fixing validation problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,17 @@ export class AppComponent {
   title = 'Hello from BridgeLabz';
   imgUrl = "../assets/BL_logo.jpg";
   url = "https://bridgelabz.com";
-  userName: string = "";
+  userName = "";
+  nameError = "";
 
+  onInput() {
+    const nameRegix = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegix.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!"
+  }
 
   onClick() {
     window.open(this.url);
